206bcc3455727372ed07b1d19425fa4712195715
Htet Lin <htetlin099798@gmail.com>
2023-12-28 05:45:35 +0630

    adding theme

--- theme/theme.ts
+++ theme/theme.ts
@@ -1,5 +1,14 @@
-// import { createTheme } from "@mui/material";
+import { createTheme } from "@mui/material";
 
-// const theme = createTheme({
-//   palette: {},
-// });
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f5f5f5",
+    },
+    secondary: {
+      main: "#606060",
+    },
+  },
+});
+
+export default theme;
