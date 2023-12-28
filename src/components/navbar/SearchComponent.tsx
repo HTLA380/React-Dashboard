e0cc66c0e5b2c60ceb4788747b7bd55de5579ac0
Htet Lin <htetlin099798@gmail.com>
2023-12-28 14:02:12 +0630

    adding navbar drawer

--- src/components/navbar/SearchComponent.tsx
+++ src/components/navbar/SearchComponent.tsx
@@ -1,13 +1,52 @@
-import React from "react";
-import { StyledIcon } from "./NavbarStyles";
-import { Search } from "@mui/icons-material";
+import * as React from "react";
+import { StyledIcon, StyledStack } from "./NavbarStyles";
+import { Close, Search } from "@mui/icons-material";
+import { Box, Button, Paper, TextField } from "@mui/material";
+import { styled } from "@mui/system";
+import { grey } from "@mui/material/colors";
 
-const SearchComponent: React.FC = () => {
+const StyledDrawer = styled(Paper)({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  right: 0,
+  padding: "2rem 5rem",
+  zIndex: 9999,
+  backgroundColor: "#e9e9e9",
+  transition: "0.3s ease-in-out",
+});
+
+export default function SearchComponents() {
+  const [open, setOpen] = React.useState(false);
+
   return (
-    <StyledIcon>
-      <Search />
-    </StyledIcon>
+    <Box>
+      <StyledIcon onClick={() => setOpen(true)}>
+        <Search />
+      </StyledIcon>
+      <StyledDrawer elevation={3} sx={{ top: open ? 0 : -200 }}>
+        <StyledStack>
+          <Search sx={{ color: grey[600] }} />
+          <TextField
+            id="standard-basic"
+            placeholder="Search..."
+            variant="standard"
+            sx={{ margin: "0 3rem 0 .5rem" }}
+            color="secondary"
+            fullWidth
+          />
+          <Button variant="contained" color="info" sx={{ marginRight: 2 }}>
+            Search
+          </Button>
+        </StyledStack>
+        <StyledIcon
+          aria-label="close"
+          onClick={() => setOpen(false)}
+          size="small"
+          sx={{ position: "absolute", top: 15, right: 20 }}>
+          <Close />
+        </StyledIcon>
+      </StyledDrawer>
+    </Box>
   );
-};
-
-export default SearchComponent;
+}
