0c8750089b08c03e9b92e68425854b4baa491c7f
Htet Lin <htetlin099798@gmail.com>
2023-12-30 12:34:41 +0630

    adding sidebar

---
+++ src/components/sidebar/DrawerComponent.tsx
@@ -0,0 +1,117 @@
+import {
+  AccountBoxRounded,
+  Dashboard,
+  LockPerson,
+  ShoppingCartCheckoutRounded,
+  WebRounded,
+} from "@mui/icons-material";
+import UserImg from "../../assets/user.png";
+import {
+  Avatar,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { blue } from "@mui/material/colors";
+import { useLocation } from "react-router-dom";
+
+const SideBarData = [
+  { name: "dashboard", icon: <Dashboard />, path: "/" },
+  {
+    name: "user",
+    icon: <AccountBoxRounded />,
+    path: "/user",
+  },
+  {
+    name: "product",
+    icon: <ShoppingCartCheckoutRounded />,
+    path: "/product",
+  },
+  {
+    name: "blog",
+    icon: <WebRounded />,
+    path: "/blog",
+  },
+  {
+    name: "login",
+    icon: <LockPerson />,
+    path: "/login",
+  },
+];
+
+const DrawerComponent = () => {
+  const location = useLocation();
+
+  return (
+    <Box sx={{ margin: "2rem 1rem" }}>
+      <Typography
+        variant="h5"
+        fontWeight={"bold"}
+        marginBottom={2}
+        sx={{ color: blue[700], cursor: "pointer" }}>
+        ReactBoard
+      </Typography>
+      <Stack
+        flexDirection={"row"}
+        alignItems={"center"}
+        gap={2}
+        sx={{
+          backgroundColor: "primary.main",
+          borderRadius: 3,
+          padding: "0.5rem",
+        }}>
+        <IconButton>
+          <Avatar
+            alt="Htet Aung Lin"
+            src={UserImg}
+            sx={{ width: 35, height: 35 }}
+          />
+        </IconButton>
+        <Typography variant="subtitle1">Htet Aung Lin</Typography>
+      </Stack>
+      <List>
+        {SideBarData.map((data) => (
+          <ListItem key={data.name} disablePadding>
+            <ListItemButton
+              href={data.path}
+              sx={{
+                backgroundColor:
+                  location.pathname === data.path ? "#1876f228" : "transparent",
+                borderRadius: 2,
+              }}>
+              <ListItemIcon
+                sx={{
+                  minWidth: "2.25rem",
+                  color:
+                    location.pathname === data.path ? "#357ab8" : "#637381",
+                }}>
+                {data.icon}
+              </ListItemIcon>
+              <ListItemText
+                primary={
+                  <Typography
+                    textTransform={"capitalize"}
+                    variant="subtitle1"
+                    sx={{
+                      color:
+                        location.pathname === data.path ? "#357ab8" : "#637381",
+                    }}>
+                    {data.name}
+                  </Typography>
+                }
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+};
+
+export default DrawerComponent;
---
+++ src/components/sidebar/SideBar.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Box, Drawer } from "@mui/material";
+import DrawerComponent from "./DrawerComponent";
+
+interface SidebarInterface {
+  drawerWidth: number;
+}
+
+const SideBar: React.FC<SidebarInterface> = ({ drawerWidth }) => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        sx={{
+          display: { xs: "block", sm: "none" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: drawerWidth,
+          },
+        }}>
+        <DrawerComponent />
+      </Drawer>
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: "none", sm: "block" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: drawerWidth,
+          },
+        }}
+        open>
+        <DrawerComponent />
+      </Drawer>
+    </Box>
+  );
+};
+
+export default SideBar;
