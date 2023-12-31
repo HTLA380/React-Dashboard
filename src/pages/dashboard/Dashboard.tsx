429e68c11dbec08e97681f2bf99ea597692ee693
Htet Lin <htetlin099798@gmail.com>
2023-12-31 11:45:59 +0630

    dashboard page

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,17 @@
+import { Container, Typography } from "@mui/material";
 import React from "react";
+import Activities from "./Activities";
 
 const Dashboard: React.FC = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Container maxWidth="xl" sx={{ paddingTop: 10 }}>
+      <Typography variant="h4" marginBottom={3}>
+        Hi, Welcome back 👋
+      </Typography>
+
+      <Activities />
+    </Container>
+  );
 };
 
 export default Dashboard;
