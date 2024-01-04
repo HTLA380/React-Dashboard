3545c545f5db7574b3f3728d4229cc7dcbd76719
Htet Lin <htetlin099798@gmail.com>
2024-01-04 19:39:00 +0630

    adding images

---
+++ src/assets/news/news.ts
@@ -0,0 +1,23 @@
+import news1 from "./news1.jpg";
+import news2 from "./news2.jpg";
+import news3 from "./news3.jpg";
+import news4 from "./news4.jpg";
+import news5 from "./news5.jpg";
+import news6 from "./news6.jpg";
+import news7 from "./news7.jpg";
+import news8 from "./news8.jpg";
+import news9 from "./news9.jpg";
+import news10 from "./news10.jpg";
+
+export {
+  news1,
+  news2,
+  news3,
+  news4,
+  news5,
+  news6,
+  news7,
+  news8,
+  news9,
+  news10,
+};
