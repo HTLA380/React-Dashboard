106aa14d438906b38f6ab6b6b695917c9eb0a8cb
Htet Lin <htetlin099798@gmail.com>
2024-01-04 20:10:32 +0630

    adding news update

---
+++ src/pages/dashboard/NewsUpdate.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { StyledCard } from "../../components/Styles";
+import { Box, Divider, Stack, Typography } from "@mui/material";
+import { newsData } from "./data";
+import { ChevronRight } from "@mui/icons-material";
+
+interface RenderNewsInterface {
+  title: string;
+  image: string;
+  description: string;
+  date: string;
+}
+
+const RenderNews: React.FC<RenderNewsInterface> = ({
+  title,
+  image,
+  description,
+  date,
+}) => {
+  return (
+    <Stack flexDirection={"row"} alignItems={"center"} gap={2} marginBottom={3}>
+      <Box
+        component="img"
+        sx={{
+          height: 50,
+          width: 50,
+          objectFit: "cover",
+          borderRadius: 2,
+        }}
+        alt="avatar"
+        src={image}
+      />
+
+      <Box
+        sx={{
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          width: "100%",
+        }}>
+        <Typography variant="body1">{title}</Typography>
+        <Typography
+          noWrap
+          color={"secondary.dark"}
+          variant="body2"
+          sx={{ width: "100%" }}>
+          {description}
+        </Typography>
+      </Box>
+      <Typography
+        variant="body2"
+        fontSize={13}
+        color={"secondary.dark"}
+        whiteSpace={"nowrap"}>
+        {date}
+      </Typography>
+    </Stack>
+  );
+};
+
+const NewsUpdate: React.FC = () => {
+  return (
+    <StyledCard>
+      <Box margin={3}>
+        <Typography variant="h5">News Update</Typography>
+      </Box>
+
+      <Box sx={{ padding: "0 1.25rem 1.25rem" }}>
+        {newsData.map((data) => (
+          <RenderNews
+            title={data.title}
+            description={data.description}
+            image={data.image}
+            date={data.date}
+          />
+        ))}
+      </Box>
+      <Divider />
+      <Stack
+        justifyContent={"end"}
+        width={"100%"}
+        flexDirection={"row"}
+        padding={1}>
+        <Stack
+          flexDirection={"row"}
+          alignItems={"center"}
+          component={"button"}
+          sx={{
+            backgroundColor: "transparent",
+            border: "none",
+            padding: "0.5rem 0rem 0.5rem 0.5rem",
+            borderRadius: 2,
+            transition: "0.15s ease-in-out",
+            cursor: "pointer",
+            ":hover": {
+              backgroundColor: "secondary.light",
+            },
+          }}>
+          <Typography>View All</Typography>
+          <ChevronRight />
+        </Stack>
+      </Stack>
+    </StyledCard>
+  );
+};
+
+export default NewsUpdate;
--- src/pages/dashboard/data.ts
+++ src/pages/dashboard/data.ts
@@ -1,3 +1,5 @@
+// ======== chart data ========  //
+
 export const ChartData = [
   {
     date: "2003",
@@ -140,5 +142,57 @@ export const radarChartData = [
     pointBorderColor: "#fff",
     pointHoverBackgroundColor: "#fff",
     pointHoverBorderColor: "rgb(54, 235, 226)",
+  },
+];
+
+// ======== News Data ========  //
+import {
+  news1,
+  news2,
+  news3,
+  news4,
+  news5,
+  news6,
+  news7,
+  news8,
+  news9,
+  news10,
+} from "../../assets/news/news";
+
+export const newsData = [
+  {
+    title: "He taught me a code. To survive.",
+    description:
+      "Tell him time is of the essence. This man is a knight in shining armor. I've lived in darkness a long time. Over the years my eyes adjusted until the dark became my world and I could see.",
+    image: news4,
+    date: "about 2 hours ago",
+  },
+  {
+    title: "I'm really more an apartment person.",
+    description:
+      "I'm going to tell you something that I've never told anyone before. Hello, Dexter Morgan. Finding a needle in a haystack isn't hard when every straw is computerized.",
+    image: news5,
+    date: "about 17 hours ago",
+  },
+  {
+    title: "Okay, I like a challenge.",
+    description:
+      "I'm Santa Claus! That's not soon enough! With gusto. Of all the friends I've had… you're the first. Who am I making this out to?",
+    image: news6,
+    date: "about 9 hours ago",
+  },
+  {
+    title: "I feel like I just went ten rounds with mighty Thor.",
+    description:
+      "Hey, whatcha watching? Dear God, they'll be killed on our doorstep! And there's no trash pickup until January 3rd. No. We're on the top. Bender, you risked your life to save me! ",
+    image: news7,
+    date: "about 3 hours ago",
+  },
+  {
+    title: "I can explain. It's very valuable.",
+    description:
+      "Hey, you add a one and two zeros to that or we walk! Enough about your promiscuous mother, Hermes! We have bigger problems.",
+    image: news9,
+    date: "about 4 hours ago",
   },
 ];
