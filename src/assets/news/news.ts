b0af8a0163867d6fb9cd042f3d608f28ad733c8a
Htet Lin <htetlin099798@gmail.com>
2024-01-10 19:47:30 +0630

    changing file names

--- src/assets/news/news.ts
+++ src/assets/news/news.ts
@@ -3,21 +3,5 @@ import news2 from "./news2.jpg";
 import news3 from "./news3.jpg";
 import news4 from "./news4.jpg";
 import news5 from "./news5.jpg";
-import news6 from "./news6.jpg";
-import news7 from "./news7.jpg";
-import news8 from "./news8.jpg";
-import news9 from "./news9.jpg";
-import news10 from "./news10.jpg";
 
-export {
-  news1,
-  news2,
-  news3,
-  news4,
-  news5,
-  news6,
-  news7,
-  news8,
-  news9,
-  news10,
-};
+export { news1, news2, news3, news4, news5 };
---
+++ src/assets/users/userImg.ts
@@ -0,0 +1,7 @@
+import user from "./user1.jpg";
+import user2 from "./user2.jpg";
+import user3 from "./user3.jpg";
+import user4 from "./user4.jpg";
+import user5 from "./user5.jpg";
+
+export { user, user2, user3, user4, user5 };
