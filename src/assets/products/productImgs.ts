f0aa7b3ee7cf194680347cb26ba33a55bdcd3f43
Htet Lin <htetlin099798@gmail.com>
2024-01-13 13:56:56 +0630

    adding product images

---
+++ src/assets/products/productImgs.ts
@@ -0,0 +1,51 @@
+import product1 from "./product_1.jpg";
+import product2 from "./product_2.jpg";
+import product3 from "./product_3.jpg";
+import product4 from "./product_4.jpg";
+import product5 from "./product_5.jpg";
+import product6 from "./product_6.jpg";
+import product7 from "./product_7.jpg";
+import product8 from "./product_8.jpg";
+import product9 from "./product_9.jpg";
+import product10 from "./product_10.jpg";
+import product11 from "./product_11.jpg";
+import product12 from "./product_12.jpg";
+import product13 from "./product_13.jpg";
+import product14 from "./product_14.jpg";
+import product15 from "./product_15.jpg";
+import product16 from "./product_16.jpg";
+import product17 from "./product_17.jpg";
+import product18 from "./product_18.jpg";
+import product19 from "./product_19.jpg";
+import product20 from "./product_20.jpg";
+import product21 from "./product_21.jpg";
+import product22 from "./product_22.jpg";
+import product23 from "./product_23.jpg";
+import product24 from "./product_24.jpg";
+
+export {
+  product1,
+  product2,
+  product3,
+  product4,
+  product5,
+  product6,
+  product7,
+  product8,
+  product9,
+  product10,
+  product11,
+  product12,
+  product13,
+  product14,
+  product15,
+  product16,
+  product17,
+  product18,
+  product19,
+  product20,
+  product21,
+  product22,
+  product23,
+  product24,
+};
