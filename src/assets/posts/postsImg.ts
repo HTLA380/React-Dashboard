0f2f63bd5820636805875ade4b057533f13589cf
Htet Lin <htetlin099798@gmail.com>
2024-01-16 05:46:27 +0630

    adding posts images

---
+++ src/assets/posts/postsImg.ts
@@ -0,0 +1,51 @@
+import post from "./cover_1.jpg";
+import post2 from "./cover_2.jpg";
+import post3 from "./cover_3.jpg";
+import post4 from "./cover_4.jpg";
+import post5 from "./cover_5.jpg";
+import post6 from "./cover_6.jpg";
+import post7 from "./cover_7.jpg";
+import post8 from "./cover_8.jpg";
+import post9 from "./cover_t9.jpg";
+import post10 from "./cover_10.jpg";
+import post11 from "./cover_11.jpg";
+import post12 from "./cover_12.jpg";
+import post13 from "./cover_13.jpg";
+import post14 from "./cover_14.jpg";
+import post15 from "./cover_15.jpg";
+import post16 from "./cover_16.jpg";
+import post17 from "./cover_17.jpg";
+import post18 from "./cover_18.jpg";
+import post19 from "./cover_19.jpg";
+import post20 from "./cover_20.jpg";
+import post21 from "./cover_21.jpg";
+import post22 from "./cover_22.jpg";
+import post23 from "./cover_23.jpg";
+import post24 from "./cover_24.jpg";
+
+export {
+  post,
+  post2,
+  post3,
+  post4,
+  post5,
+  post6,
+  post7,
+  post8,
+  post9,
+  post10,
+  post11,
+  post12,
+  post13,
+  post14,
+  post15,
+  post16,
+  post17,
+  post18,
+  post19,
+  post20,
+  post21,
+  post22,
+  post23,
+  post24,
+};
--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,12 +1,41 @@
-import { Toolbar, Typography } from "@mui/material";
+import { Add } from "@mui/icons-material";
+import { Button, Container, Stack, Typography } from "@mui/material";
 import React from "react";
 
 const Blog: React.FC = () => {
   return (
-    <>
-      <Toolbar />
-      <Typography variant="h1">Blog</Typography>
-    </>
+    <Container maxWidth="lg">
+      <Stack
+        flexDirection={"row"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        marginBottom={5}>
+        <Typography
+          variant="h3"
+          sx={{
+            fontSize: { xs: "1.4rem", sm: "1.6rem" },
+          }}>
+          Blog
+        </Typography>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "primary.contrastText",
+            color: "primary.main",
+            textTransform: "capitalize",
+            py: 1,
+            borderRadius: 2,
+            ":hover": {
+              backgroundColor: "primary.contrastText",
+            },
+          }}>
+          <Add fontSize="small" />
+          <Typography component={"span"} marginLeft={0.2}>
+            New Post
+          </Typography>
+        </Button>
+      </Stack>
+    </Container>
   );
 };
 
