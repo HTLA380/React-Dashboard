d2bf4ea70fcac3651ab2bebd3d0fc9719543f05a
Htet Lin <htetlin099798@gmail.com>
2024-01-23 15:15:08 +0630

    refactor: move images to a new directory

--- src/assets/news/news.ts
+++
@@ -1,7 +0,0 @@
-import news1 from "./news1.jpg";
-import news2 from "./news2.jpg";
-import news3 from "./news3.jpg";
-import news4 from "./news4.jpg";
-import news5 from "./news5.jpg";
-
-export { news1, news2, news3, news4, news5 };
